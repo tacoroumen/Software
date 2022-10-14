4583e007d0e812f077272d8d136b2237ba8fbf61
Tortar <tacoroumen@gmail.com>
2022-10-14 12:03:29 +0200

    uploaded blackjack

---
+++ Wedstrijd/Blackjack/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Training_Blackjack__light_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Deal = new System.Windows.Forms.Button();
+            this.Hit = new System.Windows.Forms.Button();
+            this.Stand = new System.Windows.Forms.Button();
+            this.CardVallue = new System.Windows.Forms.Label();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.Restart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Deal
+            // 
+            this.Deal.Location = new System.Drawing.Point(356, 278);
+            this.Deal.Name = "Deal";
+            this.Deal.Size = new System.Drawing.Size(75, 23);
+            this.Deal.TabIndex = 0;
+            this.Deal.Text = "Deal";
+            this.Deal.UseVisualStyleBackColor = true;
+            this.Deal.Click += new System.EventHandler(this.Deal_Click);
+            // 
+            // Hit
+            // 
+            this.Hit.Location = new System.Drawing.Point(430, 307);
+            this.Hit.Name = "Hit";
+            this.Hit.Size = new System.Drawing.Size(75, 23);
+            this.Hit.TabIndex = 1;
+            this.Hit.Text = "Hit";
+            this.Hit.UseVisualStyleBackColor = true;
+            this.Hit.Visible = false;
+            this.Hit.Click += new System.EventHandler(this.Hit_Click);
+            // 
+            // Stand
+            // 
+            this.Stand.Location = new System.Drawing.Point(282, 307);
+            this.Stand.Name = "Stand";
+            this.Stand.Size = new System.Drawing.Size(75, 23);
+            this.Stand.TabIndex = 2;
+            this.Stand.Text = "Stand";
+            this.Stand.UseVisualStyleBackColor = true;
+            this.Stand.Visible = false;
+            this.Stand.Click += new System.EventHandler(this.Stand_Click);
+            // 
+            // CardVallue
+            // 
+            this.CardVallue.AutoSize = true;
+            this.CardVallue.Location = new System.Drawing.Point(385, 196);
+            this.CardVallue.Name = "CardVallue";
+            this.CardVallue.Size = new System.Drawing.Size(16, 17);
+            this.CardVallue.TabIndex = 3;
+            this.CardVallue.Text = "0";
+            this.CardVallue.Click += new System.EventHandler(this.CardVallue_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Restart
+            // 
+            this.Restart.Location = new System.Drawing.Point(356, 346);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(75, 23);
+            this.Restart.TabIndex = 4;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Visible = false;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.CardVallue);
+            this.Controls.Add(this.Stand);
+            this.Controls.Add(this.Hit);
+            this.Controls.Add(this.Deal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Deal;
+        private System.Windows.Forms.Button Hit;
+        private System.Windows.Forms.Button Stand;
+        private System.Windows.Forms.Label CardVallue;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button Restart;
+    }
+}
+
---
+++ Wedstrijd/Blackjack/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Training_Blackjack__light_
+{
+    public partial class Form1 : Form
+    {
+        public int[] cardArray = { 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11 };
+        public Random r = new Random();
+        public int TotalCardValPlay;
+        public int TotalCardValBank;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int CardVal(int totalcardval)
+        {
+            int rnd = r.Next(0, 52);
+            int randomCardValue = cardArray[rnd];
+            while (randomCardValue == 0)
+            {   
+                rnd = r.Next(0, 52);
+                randomCardValue = cardArray[rnd];
+            }
+            if (randomCardValue == 11)
+            {
+                if (randomCardValue + totalcardval > 21)
+                {
+                    randomCardValue = 1;
+                }
+            }
+            cardArray[rnd] = 0;
+            int finalCardValue = randomCardValue;
+            return finalCardValue;
+        }
+
+        private void CardVallue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Deal_Click(object sender, EventArgs e)
+        {
+            int randomCardValue = CardVal(TotalCardValPlay);
+            TotalCardValPlay += randomCardValue;
+
+            randomCardValue = CardVal(TotalCardValBank);
+            TotalCardValBank += randomCardValue;
+
+            CardVallue.Text = Convert.ToString(TotalCardValPlay);
+            Deal.Visible = false;
+            Stand.Visible = true;
+            Hit.Visible = true;
+        }
+
+        private void Stand_Click(object sender, EventArgs e)
+        {
+            Stand.Visible = false;
+            Hit.Visible = false;
+            while (TotalCardValBank < 17)
+            {
+                int randomCardValue = CardVal(TotalCardValBank);
+                TotalCardValBank += randomCardValue;
+            }
+            if (TotalCardValBank > 21)
+            {
+                MessageBox.Show("The player has won because the bank went bust with a total card vallue of " + TotalCardValBank);
+            }
+            else
+            {
+                if (TotalCardValBank > TotalCardValPlay)
+                {
+                    MessageBox.Show("The bank has won with a card vallue of " + TotalCardValBank + " against the player's card vallue of " + TotalCardValPlay);
+                }
+                if (TotalCardValBank < TotalCardValPlay)
+                {
+                    MessageBox.Show("The player has won with a card vallue of " + TotalCardValPlay + " against the bank's card vallue of " + TotalCardValBank);
+                }
+                if (TotalCardValBank == TotalCardValPlay)
+                {
+                    MessageBox.Show("Neither the bank nor player has won because they both have the same card vallue. The bank's card vallue was " + TotalCardValBank + ". The player's card vallue was " + TotalCardValBank  + ".");
+                }
+            }
+            Stand.Visible = false;
+            Hit.Visible = false;
+            Restart.Visible = true;
+        }
+
+        private void Hit_Click(object sender, EventArgs e)
+        {
+            int randomCardValue = CardVal(TotalCardValPlay);
+            TotalCardValPlay += randomCardValue;
+
+            CardVallue.Text = Convert.ToString(TotalCardValPlay);
+            if (TotalCardValPlay > 21)
+            {
+                MessageBox.Show("You went bust with a total card vallue of " + TotalCardValPlay);
+                Stand.Visible = false;
+                Hit.Visible = false;
+                Restart.Visible = true;
+
+            }
+            if (TotalCardValPlay == 21)
+            {
+                MessageBox.Show("Blackjack! You won!!");
+                Stand.Visible = false;
+                Hit.Visible = false;
+                Restart.Visible = true;
+            }
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+            
+        }
+    }
+}
