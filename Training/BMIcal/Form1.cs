fe3e9ad85e4d160c1010fb1d3ea6fb8e5c2548b8
Tortar <tacoroumen@gmail.com>
2022-10-20 17:47:36 +0200

    added files

---
+++ Training/BMIcal/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Tortar : Form
+    {
+        public Tortar()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(Weight.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                Weight.Text = Weight.Text.Remove(Weight.Text.Length - 1);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float BMI;
+            float height_meter;
+            height_meter = float.Parse(height.Text) / 100;
+            BMI = float.Parse(Weight.Text) / ((height_meter) * (height_meter));
+            MessageBox.Show("Your BMI is " + BMI.ToString("0.0") + ".");
+            
+
+        }
+
+        private void height_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(Weight.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                height.Text = height.Text.Remove(height.Text.Length - 1);
+            }
+        }
+    }
+}
---
+++ Training/Batman vs Superman/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Batman_vs_Superman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int RandomNumb(int min, int max)
+        {
+            var randomnum = new Random();
+            return randomnum.Next(min, max);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int remaining = 20;
+            kracht_superman.Text = remaining.ToString();
+            kracht_batman.Text = remaining.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (laser.Checked == true)
+            {
+                int batman_remaining = int.Parse(kracht_batman.Text) - RandomNumb(1, 8);
+                kracht_batman.Text = batman_remaining.ToString();
+            }
+            if (Superkracht.Checked == true)
+            {
+                int batman_remaining = int.Parse(kracht_batman.Text) - RandomNumb(5, 7);
+                kracht_batman.Text = batman_remaining.ToString();
+            }
+            if (int.Parse(kracht_batman.Text) <= 0)
+            {
+                kracht_batman.Text = 0.ToString();
+                MessageBox.Show("Superman has won the fight.");
+                restart.Visible = true;
+
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (batarang.Checked == true)
+            {
+                int superman_remaining = int.Parse(kracht_superman.Text) - RandomNumb(1, 8);
+                kracht_superman.Text = superman_remaining.ToString();
+            }
+            if (Kungfu.Checked == true)
+            {
+                int superman_remaining = int.Parse(kracht_superman.Text) - RandomNumb(5, 7);
+                kracht_superman.Text = superman_remaining.ToString();
+            }
+            if (int.Parse(kracht_superman.Text) <= 0)
+            {
+                kracht_superman.Text = 0.ToString();
+                MessageBox.Show("batman has won the fight.");
+                restart.Visible = true;
+
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kracht_superman_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            int remaining = 20;
+            kracht_superman.Text = remaining.ToString();
+            kracht_batman.Text = remaining.ToString();
+            restart.Visible = false;
+        }
+    }
+}
